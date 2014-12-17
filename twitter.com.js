63ffbd049ee36be0a02d5219a0c73d0b2f9f8426
andy jiang <andyjiang@gmail.com>
2014-12-16 21:19:18 -0800

    added dodo to twitter

--- twitter.com.js
+++ twitter.com.js
@@ -1,3 +1,88 @@
-// Everyone you follow will be added to dodo list.
-// and muted.
+
+/**
+ * twitter.com.js
+ *
+ * README.md
+ * > Listens for your following of users, automatically adds those users
+ * > to your Dodo[1] list.
+ *
+ * deps:
+ *  jQuery 1.9
+ */
+
+/**
+ * Static variables.
+ */
+
+var host = 'https://dodo-twitter.herokuapp.com/api/';
+var userId = '';
+
+/**
+ * Send request to server to add a new dodo.
+ *
+ * @param {String} userId
+ * @param {String} dodoId
+ * @param {Function} fn (optional)
+ */
+
+function addDodo(userId, dodoId, fn) {
+  if (userid)
+    $.post(host + 'dodo', { userId: userId, dodoId: dodoId }, fn);
+}
+
+/**
+ * Return twitter user id from follow button.
+ *
+ * @param {Element} button
+ */
+
+function getDodoId(button) {
+  return $(button)
+    .closest('div.content')
+    .find('span.js-action-profile-name')
+    .attr('data-user-id');
+}
+
+/**
+ * On click for follow buttons, bind appropriate event listener.
+ */
+
+function bindFollowButtons() {
+  var buttons = document.getElementsByClassName('follow-button');
+  console.log(buttons.length);
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+      addDodo(userId, getDodoId(this), showMessage);
+    });
+  }
+}
+
+/**
+ * Show visual confirmation that dodo has been added.
+ */
+
+function showMessage(err, data) {
+  // Show something on the web page.
+  if (err) console.log(err);
+  console.log(data);
+}
+
+/**
+ * Set userId.
+ */
+
+function setUserId() {
+  userId = $('#user-dropdown').find('img.avatar').attr('data-user-id');
+  if (!userId)
+    showMessage('Please login to use Dodo.', null);
+}
+
+/**
+ * Start script!
+ */
+
+setTimeout(function() {
+  setUserId();
+  bindFollowButtons();
+}, 5000);
 
